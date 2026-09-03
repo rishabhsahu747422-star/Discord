f478892a4ae03037d77fc88df17e5294f8868041
rishabhsahu747422-star <rishabhsahu747422@gmail.com>
2026-09-03 13:37:56 +0530

    server deleted updated get all

--- src/controllers/server.controller.js
+++ src/controllers/server.controller.js
@@ -1,3 +1,4 @@
+import { log } from "console";
 import serverModel from "../models/server.model.js";
 import userModel from "../models/user.model.js";
 import sendFiles from "../services/storage.service.js";
@@ -45,7 +46,89 @@ export const createServer = async (req, res) => {
     return res.status(500).json(new ApiError(500, "Internal server error"));
   }
 };
+export const deleteServer = async (req, res) => {
+  try {
+    const { serverId } = req.params;
 
+    const server = serverModel.findOne({ serverId });
+
+    if (!server) {
+      throw new ApiError(400, "Server not Exists");
+    }
+
+    await serverModel.findByIdAndDelete({ serverId });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "Server Deleted successfully"));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+export const getSingleServer = async (req, res) => {
+  try {
+    const { serverId } = req.params;
+
+    const server = await serverModel.findById({ serverId });
+
+    if (!server) {
+      throw new ApiError(400, "Server not Exist");
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, server, "Server Fetched Succcessfully"));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+export const updateServer = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const { name, description, isPublic } = req.body;
+    const icon = req.files.icon[0];
+    const banner = req.files.banner[0];
+
+    const server = await serverModel.findByIdAndUpdate(
+      _id,
+      { name, description, isPublic, icon, banner },
+      { new: true },
+    );
+
+    if (!server) {
+      throw new ApiError(400, "Server not found");
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, server, "Server updated Successfully"));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getAllServer = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.user._id).populate("server");
+
+    if (!user || user.server.length === 0) {
+      throw new ApiError(400, "User not joined to any server");
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Servers fetched successfully",
+      servers: user.server,
+    });
+  } catch (error) {
+    console.log(error.message);
+
+    return res.status(error.statusCode || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 export const joinServer = async (req, res) => {
   try {
     const { inviteCode } = req.params;
